8b81009f36aec9ca58133721cd093c761fa19135
Adetayo Omotomiwa <adetayoomotomiwa99@gmail.com>
2020-10-25 04:06:29 -0700

    first commit

---
+++ precache-manifest.9fe66894ec85828ad39a92d4321050c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bf3d90d3b0d161d06afba8e25632c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b6c316eeb33de65e04",
+    "url": "/static/css/main.c743e94b.chunk.css"
+  },
+  {
+    "revision": "0eade9aa7e99256ab602",
+    "url": "/static/js/2.4a05ac02.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4a05ac02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b6c316eeb33de65e04",
+    "url": "/static/js/main.ae52533b.chunk.js"
+  },
+  {
+    "revision": "42236b3c5a0e9054fb96",
+    "url": "/static/js/runtime-main.0c82e416.js"
+  }
+]);
